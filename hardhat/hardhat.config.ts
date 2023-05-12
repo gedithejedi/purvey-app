32dde8200112602fae55c80c85c6374f0d1800a4
gedithejedi <gediminaspetreikis@yahoo.com>
2023-05-12 23:46:31 +0100

    restructures the folders

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config({path:__dirname+'/.env'})
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
