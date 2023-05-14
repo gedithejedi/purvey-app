2a57ca282a48d5cc7b8285f370a14cba421c3451
gedithejedi <gediminaspetreikis@yahoo.com>
2023-05-14 06:02:44 +0100

    potential fixes

--- frontend/src/components/AnonCard/Information.tsx
+++ frontend/src/components/AnonCard/Information.tsx
@@ -7,6 +7,7 @@ import { env } from "~/env.mjs";
 import { ethers } from "ethers";
 import anonCard from "~/utils/AnonCard.json";
 import { useMetaMask } from '~/hooks/useMetaMask';
+import {CONTRACT_ADDRESS} from "../../utils/constants";
 
 const NFT_STORAGE = env.NEXT_PUBLIC_NFT_STORAGE;
 const client = new NFTStorage({ token: NFT_STORAGE })
@@ -22,7 +23,6 @@ const normFile = (e: any) => {
 const AnonCardInformation = () => {
   const {state} = useMetaMask()
   const askContractToMintNft = async (metadata) => {
-    const CONTRACT_ADDRESS = "0x637FB5145070aF52095762bD6a274e4b3370B446";
     console.log(999)
     try {
       const { ethereum } = window;
--- hardhat/contracts/AnonCard.sol
+++ hardhat/contracts/AnonCard.sol
@@ -5,10 +5,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract AnonCard is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+contract AnonCard is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
